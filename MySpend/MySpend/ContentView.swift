630361f2879a4098ad2ac0d178719294d3f32ff5
Fabian Rodriguez <fabianjra@hotmail.com>
2023-06-24 16:37:32 -0600

    Upload base App

---
+++ MySpend/MySpend/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MySpend
+//
+//  Created by Fabian Rodriguez on 24/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("My Spend for iOS")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySpend/MySpend/MySpendApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpendApp.swift
+//  MySpend
+//
+//  Created by Fabian Rodriguez on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MySpendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
