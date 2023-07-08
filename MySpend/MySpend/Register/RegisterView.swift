26738d31fe1d3735f860fcffad4ee7fae9d4e287
Fabian Rodriguez <fabianjra@hotmail.com>
2023-07-07 23:04:33 -0600

    Create RegisterView base.

---
+++ MySpend/MySpend/Register/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  MySpend
+//
+//  Created by Fabian Rodriguez on 7/7/23.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text("Register View")
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
