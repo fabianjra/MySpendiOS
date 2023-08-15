fee473ecf1fdb448a241f823da6e80cde471712a
Fabian Rodriguez <fabianjra@hotmail.com>
2023-08-15 00:23:49 -0600

    Add SemiCircleShapeCurved for the TabView.

--- MySpend/MySpend/Presentation/Shapes/SemiCircleShape.swift
+++ MySpend/MySpend/Presentation/Shapes/SemiCircleShape.swift
@@ -26,11 +26,15 @@ import SwiftUI
  - Date: June 2023
  */
 struct SemiCircleShape: Shape {
-    
     func path(in rect: CGRect) -> Path {
-        
+
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.minY), radius: rect.width / 8, startAngle: .degrees(180), endAngle: .degrees(.zero), clockwise: true)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.minY),
+                    radius: rect.width / 8,
+                    startAngle: .degrees(180),
+                    endAngle: .degrees(.zero),
+                    clockwise: true)
+        
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
         path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
@@ -38,5 +42,57 @@ struct SemiCircleShape: Shape {
         path.closeSubpath()
         
         return path
+    }
+}
+
+struct SemiCircleShapeCurved: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        let h = rect.maxY * 0.7
+        
+        var path = Path()
+        path.move(to: .zero)
+        
+        path.addLine(to: CGPoint(x: rect.midX / 2.0, y: rect.minY))
+        
+        path.addCurve(to: CGPoint(x: rect.midX, y: h),
+                      control1: CGPoint(x: rect.midX * 0.8, y: rect.minY),
+                      control2: CGPoint(x: rect.midX * 0.7, y: h))
+        
+        path.addCurve(to: CGPoint(x: rect.midX * 3.0 / 2.0, y: rect.minY),
+                      control1: CGPoint(x: rect.midX * 1.3, y: h),
+                      control2: CGPoint(x: rect.midX * 1.2, y: rect.minY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+struct SemiCircleShape_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            
+            Color.background
+            
+            VStack {
+                Color.tabViewBackground
+                    .clipShape(SemiCircleShape())
+                    .frame(height: Frames.tabViewHeight)
+            }
+            .padding(.bottom, 200)
+            
+            VStack {
+                Color.tabViewBackground
+                    .clipShape(SemiCircleShapeCurved())
+                    .frame(height: Frames.tabViewHeight)
+            }
+            .padding(.top, 200)
+
+        }
     }
 }
