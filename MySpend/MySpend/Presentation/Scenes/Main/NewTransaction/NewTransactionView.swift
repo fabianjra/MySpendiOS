929162e7a99db8438ef06e25f1a04f921ada615f
Fabian Rodriguez <fabianjra@hotmail.com>
2024-09-08 22:43:41 -0600

    Se agrega el NavigationStack a la vista modal de NewTransaction para mostrar el toolbarItem del keyboard para navegar entre TextFields

--- MySpend/MySpend/Presentation/Scenes/Main/NewTransaction/NewTransactionView.swift
+++ MySpend/MySpend/Presentation/Scenes/Main/NewTransaction/NewTransactionView.swift
@@ -17,130 +17,131 @@ struct NewTransactionView: View {
     let notesId = "notes"
     
     var body: some View {
-        ScrollViewReader { scrollViewProxy in
-            FormContainer {
-                
-                HeaderNavigator(title: "New transaction",
-                                titleWeight: .regular,
-                                titleSize: .bigXL,
-                                subTitle: "Enter the new transation details",
-                                onlyTitle: true)
-                .padding(.vertical)
-                
-                
-                // MARK: SEGMENT
-                VStack {
-                    PickerSegmented(selection: $viewModel.model.transactionType,
-                                    segments: TransactionTypeEnum.allCases)
-                    .padding(.bottom)
-                }
-                
-                // MARK: DATE
-                VStack {
-                    TextFieldReadOnly(text: $viewModel.model.dateString,
-                                      iconLeading: Image.calendar,
-                                      colorDisabled: false)
-                    .onTapGesture {
-                        viewModel.showDatePicker = true
+        NavigationStack { // Needed to show toolBar Keyboard.
+            ScrollViewReader { scrollViewProxy in
+                FormContainer {
+                    
+                    HeaderNavigator(title: "New transaction",
+                                    titleWeight: .regular,
+                                    titleSize: .bigXL,
+                                    subTitle: "Enter the new transation details",
+                                    onlyTitle: true)
+                    .padding(.vertical)
+                    
+                    
+                    // MARK: SEGMENT
+                    VStack {
+                        PickerSegmented(selection: $viewModel.model.transactionType,
+                                        segments: TransactionTypeEnum.allCases)
+                        .padding(.bottom)
                     }
-                    .sheet(isPresented: $viewModel.showDatePicker) {
-                        NavigationView {
-                            
-                            DatePicker("",
-                                       selection: $viewModel.model.selectedDate,
-                                       displayedComponents: .date)
-                            .padding(.horizontal)
-                            .datePickerStyle(.graphical)
-                            .onChange(of: viewModel.model.selectedDate, { oldValue, newValue in
-                                viewModel.model.dateString = Utils.dateToStringShort(date: newValue)
-                                //let day = selectedDate.formatted(.dateTime.day())
-                            })
-                            .toolbar {
-                                ToolbarItem(placement: .cancellationAction) {
-                                    Button("Today") {
-                                        viewModel.model.selectedDate = .now
-                                    }
-                                    .padding()
-                                    .padding(.top)
-                                }
+                    
+                    // MARK: DATE
+                    VStack {
+                        TextFieldReadOnly(text: $viewModel.model.dateString,
+                                          iconLeading: Image.calendar,
+                                          colorDisabled: false)
+                        .onTapGesture {
+                            viewModel.showDatePicker = true
+                        }
+                        .sheet(isPresented: $viewModel.showDatePicker) {
+                            NavigationView {
                                 
-                                ToolbarItem(placement: .confirmationAction) {
-                                    Button("Done") {
-                                        viewModel.showDatePicker = false
+                                DatePicker("",
+                                           selection: $viewModel.model.selectedDate,
+                                           displayedComponents: .date)
+                                .padding(.horizontal)
+                                .datePickerStyle(.graphical)
+                                .onChange(of: viewModel.model.selectedDate, { oldValue, newValue in
+                                    viewModel.model.dateString = Utils.dateToStringShort(date: newValue)
+                                    //let day = selectedDate.formatted(.dateTime.day())
+                                })
+                                .toolbar {
+                                    ToolbarItem(placement: .cancellationAction) {
+                                        Button("Today") {
+                                            viewModel.model.selectedDate = .now
+                                        }
+                                        .padding()
+                                        .padding(.top)
                                     }
-                                    .padding()
-                                    .padding(.top)
+                                    
+                                    ToolbarItem(placement: .confirmationAction) {
+                                        Button("Done") {
+                                            viewModel.showDatePicker = false
+                                        }
+                                        .padding()
+                                        .padding(.top)
+                                    }
                                 }
                             }
+                            .presentationCornerRadius(ConstantRadius.cornersModal)
+                            .presentationDetents([.height(ConstantFrames.calendarHeight)])
+                            
                         }
-                        .presentationCornerRadius(ConstantRadius.cornersModal)
-                        .presentationDetents([.height(ConstantFrames.calendarHeight)])
-                        
                     }
-                }
-                
-                
-                // MARK: TEXTFIELDS
-                VStack {
-                    TextField("", text: $viewModel.model.amount, prompt:
-                                Text("Amount").foregroundColor(.textFieldPlaceholder))
-                    .textFieldStyle(TextFieldIconStyle($viewModel.model.amount,
-                                                       iconLeading: Image.dolarSquareFill,
-                                                       textLimit: ConstantViews.amoutMaxLength,
-                                                       errorMessage: $viewModel.errorMessage))
-                    .keyboardType(.decimalPad)
-                    .focused($focusedField, equals: .amount)
+                    
+                    
+                    // MARK: TEXTFIELDS
+                    VStack {
+                        TextField("", text: $viewModel.model.amount, prompt:
+                                    Text("Amount").foregroundColor(.textFieldPlaceholder))
+                        .textFieldStyle(TextFieldIconStyle($viewModel.model.amount,
+                                                           iconLeading: Image.dolarSquareFill,
+                                                           textLimit: ConstantViews.amoutMaxLength,
+                                                           errorMessage: $viewModel.errorMessage))
+                        .keyboardType(.decimalPad)
+                        .focused($focusedField, equals: .amount)
+                        
+                        
+                        //TODO: Change to sheet list (loading and showing all categories).
+                        TextField("", text: $viewModel.model.categoryId, prompt:
+                                    Text("Category").foregroundColor(.textFieldPlaceholder))
+                        .textFieldStyle(TextFieldIconStyle($viewModel.model.categoryId,
+                                                           iconLeading: Image.stackFill,
+                                                           errorMessage: $viewModel.errorMessage))
+                        .focused($focusedField, equals: .category)
+                        .onSubmit { process() }
+                        
+                        
+                        TextField("", text: $viewModel.model.notes, prompt:
+                                    Text("Notes").foregroundColor(.textFieldPlaceholder))
+                        .textFieldStyle(TextFieldIconStyle($viewModel.model.notes,
+                                                           errorMessage: $viewModel.errorMessage))
+                        .id(notesId)
+                        .focused($focusedField, equals: .notes)
+                        .padding(.bottom)
+                        .onSubmit { process() }
+                    }
                     .modifier(AddKeyboardToolbar(focusedField: $focusedField))
                     
                     
-                    //TODO: Change to sheet list (loading and showing all categories).
-                    TextField("", text: $viewModel.model.categoryId, prompt:
-                                Text("Category").foregroundColor(.textFieldPlaceholder))
-                    .textFieldStyle(TextFieldIconStyle($viewModel.model.categoryId,
-                                                       iconLeading: Image.stackFill,
-                                                       errorMessage: $viewModel.errorMessage))
-                    .focused($focusedField, equals: .category)
-                    .onSubmit { process() }
+                    // MARK: BUTTONS
                     
-                    
-                    TextField("", text: $viewModel.model.notes, prompt:
-                                Text("Notes").foregroundColor(.textFieldPlaceholder))
-                    .textFieldStyle(TextFieldIconStyle($viewModel.model.notes,
-                                                       errorMessage: $viewModel.errorMessage))
-                    .id(notesId)
-                    .focused($focusedField, equals: .notes)
-                    .padding(.bottom)
-                    .onSubmit { process() }
-                }
-                .modifier(AddKeyboardToolbar(focusedField: $focusedField)) //TODO: No se esta mostrando
-                
-                
-                // MARK: BUTTONS
-                
-                VStack {
-                    Button("Accept") {
-                        process()
-                    }
-                    .buttonStyle(ButtonPrimaryStyle(isLoading: $viewModel.isLoading))
-                    .padding(.vertical)
-                    .padding(.bottom)
-                    
-                    
-                    TextError(message: viewModel.errorMessage)
-                }
-                
-                Spacer()
-            }
-            .onAppear {
-                viewModel.onAppear()
-            }
-            .onChange(of: focusedField) { _, newFocusedField in
-                if focusedField == .notes {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-                        withAnimation {
-                            scrollViewProxy.scrollTo(notesId, anchor: .bottom)
+                    VStack {
+                        Button("Accept") {
+                            process()
                         }
+                        .buttonStyle(ButtonPrimaryStyle(isLoading: $viewModel.isLoading))
+                        .padding(.vertical)
+                        .padding(.bottom)
                         
+                        
+                        TextError(message: viewModel.errorMessage)
+                    }
+                    
+                    Spacer()
+                }
+                .onAppear {
+                    viewModel.onAppear()
+                }
+                .onChange(of: focusedField) { _, newFocusedField in
+                    if focusedField == .notes {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                            withAnimation {
+                                scrollViewProxy.scrollTo(notesId, anchor: .bottom)
+                            }
+                            
+                        }
                     }
                 }
             }
