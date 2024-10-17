17b2b78fa3a4b363dbda9309a6b46fa6ecd34327
Fabian Rodriguez <fabianjra@hotmail.com>
2024-10-16 18:56:11 -0600

    Se finaliza la docuemntacion de addNewDocument.

--- MySpend/MySpend/Data/Repository.swift
+++ MySpend/MySpend/Data/Repository.swift
@@ -16,6 +16,12 @@ struct Repository: UserValidationProtocol {
      It uses Firebase to generate a new document ID automatically.
      The operation is asynchronous and throws errors if something goes wrong during the process.
      
+     - The function first validates the current user's credentials and retrieves the user's ID.
+     - It then creates a reference to the specified subcollection for the authenticated user.
+     - The given model is encoded into a Firestore-compatible format.
+     - Firebase generates a unique document ID automatically when the new document is added to the subcollection using `addDocument.
+
+     
      **Example:**
      ```swift
      private var listener: ListenerRegistration?
@@ -36,12 +42,12 @@ struct Repository: UserValidationProtocol {
      ```
      
      - Parameters:
-        - model: A generic parameter T conforming to Codable. This is the model object that will be encoded and stored as a document in Firestore.
-        - collection: A value of the enum type CollectionsFB that specifies the Firestore subcollection where the new document will be added.
+        - model: A generic parameter `T` conforming to `Codable`. This is the model object that will be encoded and stored as a document in Firestore.
+        - collection: A value of the enum type `CollectionsFB` that specifies the Firestore subcollection where the new document will be added.
      
      - Returns: ListenerRegistration opcional (propiedad de Firebase)
      
-     - Throws: Manejo de escepciones por medio de banderas.
+     - Throws: The function throws an error if: 1: The current user validation fails. 2: The model cannot be encoded properly. 3: There are issues when interacting with Firestore (e.g., network issues or permission errors).
      
      - Authors: Fabian Rodriguez
      
