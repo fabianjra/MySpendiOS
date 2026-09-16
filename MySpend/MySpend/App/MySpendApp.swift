6e32a83cfaf21fa55d8ea5099b9af4d1f9088907
FabianJRA <fabianjra@hotmail.com>
2026-09-16 14:28:18 -0600

    Se elimina Firebase || Se actualiza a iOS27

--- MySpend/MySpend/App/MySpendApp.swift
+++ MySpend/MySpend/App/MySpendApp.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import Firebase
+//import Firebase
 
 //Resolve warning: [GoogleUtilities/AppDelegateSwizzler][I-SWZ001014] App Delegate does not conform to UIApplicationDelegate protocol.
 //It is not a good idea to go back to AppDelegate.
@@ -34,10 +34,10 @@ struct MySpendApp: App {
     
     
     //Se agrega la dependencia de Firebase aqui, ya que este archivo realiza la misma funcion que en el AppDelegate.
-    init() {
+    //init() {
         //FirebaseConfiguration.shared.setLoggerLevel(.min) //Evitar mensajes de firebase innecesarios.
-        FirebaseApp.configure()
-    }
+        //FirebaseApp.configure()
+    //}
 
     var body: some Scene {
         WindowGroup {
--- MySpend/MySpend/Utilities/Logger.swift
+++ MySpend/MySpend/Utilities/Logger.swift
@@ -6,8 +6,8 @@
 //
 
 import Foundation
-import FirebaseCrashlytics
-import FirebaseAnalytics
+//import FirebaseCrashlytics
+//import FirebaseAnalytics
 
 enum ErrorType {
     case none
@@ -34,19 +34,19 @@ struct Logger {
                                     function: String = #function,
                                     line: Int = #line) {
 
-        let crashlytics = Crashlytics.crashlytics()
+        //let crashlytics = Crashlytics.crashlytics()
 
         // Log a breadcrumb for quick filtering in the Crashlytics console
         let fileName = file.components(separatedBy: "/").last ?? file
-        crashlytics.log("CatchException – \(fileName):\(line) – \(function)")
+        //crashlytics.log("CatchException – \(fileName):\(line) – \(function)")
 
         // Attach source context as custom keys (visible in the stack-trace panel)
-        crashlytics.setCustomValue((file as NSString).lastPathComponent, forKey: "file")
-        crashlytics.setCustomValue(function, forKey: "function")
-        crashlytics.setCustomValue(line, forKey: "line")
+        //crashlytics.setCustomValue((file as NSString).lastPathComponent, forKey: "file")
+        //crashlytics.setCustomValue(function, forKey: "function")
+        //crashlytics.setCustomValue(line, forKey: "line")
 
         // Record the error; Crashlytics will group identical exceptions
-        crashlytics.record(error: error)
+        //crashlytics.record(error: error)
     }
 
     
@@ -87,7 +87,7 @@ struct Logger {
                                line: Int = #line) {
         
         let logId = UUID().uuidString
-        print("********** START EXCEPTION LOG: \(logId) **********")
+        print("********** START EXCEPTION LOG: \(logId)")
         print("Handled catch in: \(file.components(separatedBy: "/").last ?? file), function: \(function), line: \(line), description: \(error.localizedDescription)")
         
         if UtilsUI.useAnalytics {
@@ -102,7 +102,7 @@ struct Logger {
             CoreDataCatchException(error)
         }
         
-        print("********** END EXCEPTION LOG: \(logId) **********")
+        print("********** END EXCEPTION LOG.")
     }
     
     private static func CoreDataCatchException(_ error: Error) {
@@ -172,21 +172,21 @@ struct Logger {
         
         //#if DEBUG
         let logId = UUID().uuidString
-        print("********** START CUSTOM MESSSAG LOG: \(logId) **********")
+        print("********** START CUSTOM MESSSAG LOG: \(logId)")
         let message = "-Print from: \(fileName), function: \(function), line: \(line) \n-Description: \(obj)"
         print(message)
-        print("********** END CUSTOM MESSSAG LOG: \(logId) **********")
+        print("********** END CUSTOM MESSSAG LOG.")
         
         //#else
         
         if UtilsUI.useAnalytics {
             // Para el titulo: Máximo 40 caracteres, minúsculas, sin espacios.
-            Analytics.logEvent("ios_custom_log", parameters: [
-                "file": fileName,
-                "function": function,
-                "line": line,
-                "message": String(describing: obj)
-            ])
+//            Analytics.logEvent("ios_custom_log", parameters: [
+//                "file": fileName,
+//                "function": function,
+//                "line": line,
+//                "message": String(describing: obj)
+//            ])
         }
         //#endif
     }
